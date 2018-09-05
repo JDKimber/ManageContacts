83c94a6c715e2645b4975e66ab997bb27212ffad
JDKimber <jdkimber@yahoo.com>
2018-09-05 17:45:46 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact name="Jon Doe" email="jdoe@gmail.com" phoen="555-555-5555" />
+      </div>
+    );
+  }
+}
+
+export default App;
